9a4adfc40f4beb7a83c6c76c5ca2e9bb8098f06c
ArkejGit <robert.krawczyk92@gmail.com>
2017-12-19 18:21:07 +0100

    Modify mock config to include also main module

--- config/mockr/default.js
+++ config/mockr/default.js
@@ -3,7 +3,7 @@
 const cwd = process.cwd();
 const config = {};
 
-config[ `${cwd}/tests/factory.js` ] = {
+config[ `${cwd}/tests/factory.js` ] = config[ `${cwd}/tests/module.js` ] = {
 		'bemquery-selector-engine': `${cwd}/tests/support/mocks/bemquery-selector-engine.js`
 };
 
